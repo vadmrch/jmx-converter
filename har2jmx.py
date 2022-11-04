f95c6c0fc4bf5b87190ae114e48ce2a2dd828255
Vadim Murych <vadmrch@gmail.com>
2022-11-04 20:09:13 +0300

    Change url parsing

--- har2jmx.py
+++ har2jmx.py
@@ -16,6 +16,13 @@ def read_har(path):
     return har_dict
 
 
+def make_jmx_name(path):
+    har_name = path.replace('\\', '/').split('/')[-1].split('.')[0]
+    timestamp = datetime.now().strftime('%Y-%m-%d_%H%M%S')
+    jmx_name = f'{har_name}_{timestamp}.jmx'
+    return jmx_name
+
+
 # Check content type basen on Content-type header
 def ct_header_check(entry):
     for response_header in entry['response']['headers']:
@@ -37,35 +44,85 @@ def ct_url_check(entry):
         return False
 
 
-def request_parser(request):
-    request_protocol = re.search('^https|^http', request['url']).group(0)
-    request_method = request['method']
-    request_host = request['url'].split('/')[2]
+def params_parser(params):
+    params_list = []
+    for param in params:
+        split_param = param.split('=')
+        # For nameless parameters
+        if len(split_param) > 1:
+            param_name = split_param[0]
+            param_value = split_param[1]
+        else:
+            param_name = None
+            param_value = split_param[0]
+
+        param_dict = {'name': param_name,
+                      'value': param_value}
+        params_list.append(param_dict)
+    return params_list
+
+
+def url_parser(request):
     try:
-        request_path = '/' + '/'.join(request['url'].split('/')[3:]).split('?')[0]
+        request_protocol = re.search('^(.+?)://', request['url']).group(1)
+    except AttributeError:
+        request_protocol = None
+
+    split_url = request['url'].split('/')
+    # Adjust index at url parsing in case there is no protocol
+    if request_protocol:
+        i = 2
+    else:
+        i = 0
+    request_host = split_url[i]
+
+    # In case there is Port in url
+    if ':' in request_host:
+        request_port = request_host.split(':')[1]
+        request_host = request_host.split(':')[0]
+    else:
+        request_port = None
+
+    try:
+        request_path = '/' + '/'.join(split_url[i+1:]).split('?')[0]
         request_params = request['url'].split('?')[1].split('&')
     except IndexError:
-        request_path = '/' + '/'.join(request['url'].split('/')[3:])
-        request_params = []
-    try:
-        request_body = request['postData']['text']
-    except KeyError:
+        request_path = '/' + '/'.join(split_url[i+1:])
+        request_params = None
+    # Convert list ['name=value'...] to list of dictionaries [{'name':name, 'value':value}...] for more comfort later
+    if request_params:
+        request_params = params_parser(request_params)
+
+    if request['bodySize'] > 0:
+        try:
+            request_body = request['postData']['text']
+        except KeyError:
+            request_body = 'Failed to parse body data, but it has to be here'
+    else:
         request_body = None
-    return request_protocol, request_method, request_host, request_path, request_params, request_body
+
+    parsed_url = {
+        'method': request['method'],
+        'protocol': request_protocol,
+        'host': request_host,
+        'port': request_port,
+        'path': request_path,
+        'params': request_params,
+        'body': request_body
+    }
+    return parsed_url
 
 
 def main(args):
     har = read_har(args.har_file)
-    har_name = args.har_file.replace('\\', '/').split('/')[-1].split('.')[0]
-    timestamp = datetime.now().strftime('%Y-%m-%d_%H%M%S')
-    jmx_name = f'{har_name}_{timestamp}.jmx'
+    jmx_name = make_jmx_name(args.har_file)
     for entry in har['log']['entries']:
         if not args.no_filter:
             if ct_url_check(entry):
                 continue
             if ct_header_check(entry):
                 continue
-        print(request_parser(entry['request']))
+        print(url_parser(entry['request']))
     print(jmx_name)
 
 
